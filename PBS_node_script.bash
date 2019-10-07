d1bca27b6a7ab5e7cd20b4018b0c79a3b90798f6
Mark Panning <Mark.P.Panning@jpl.nasa.gov>
2019-10-07 11:37:43 -0700

    Added in functionality for running on halo

---
+++ PBS_node_script.bash
@@ -0,0 +1,23 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=16
+#PBS -l walltime=48:00:00
+#PBS -N MCMC_test
+
+export directory=/nobackup/planetseismology/panning/
+export SRC_DIR=/home/panning/git/tremor
+export program="$SRC_DIR/MCMC_main.py"
+source /home/panning/.bashrc
+conda activate obspy
+which python
+# conda info
+export PBS_O_WORKDIR=$directory
+cd $PBS_O_WORKDIR
+
+export STARTOFFSET=16
+for i in {1..16}; do
+    echo "Launching " $[i+$STARTOFFSET]
+    echo python $program -d $directory -p $[i+$STARTOFFSET]
+    python $program -d $directory -p $[i+$STARTOFFSET] &
+    # python $program --version
+done
+wait
